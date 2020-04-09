efe1aa2d36ca453c63903061647dbf8264f2cf4e
Cecilia Avery <cilsong@gmail.com>
2020-04-09 00:05:11 -0400

    rewrite apis with postgres db

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 type Book struct {
 	ID int `json:id`
 	Title string `json:title`
-	Author string `jsong:author`
+	Author string `json:author`
 	Year string `json:year`
 }
 
@@ -88,8 +88,8 @@ func addBook(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&book)
 
 	err := db.QueryRow("insert into books (title, author, year) values($1, $2, $3) RETURNING id;", book.Title, book.Author, book.Year).Scan(&bookID)
-	log.Fatal(err)
 
+	logFatal(err)
 	json.NewEncoder(w).Encode(bookID)
 }
 
@@ -97,13 +97,19 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 	var book Book
 	json.NewDecoder(r.Body).Decode(&book)
 
-	result, err := db.Exec("update books set title=$1, author=$2, year=$3 RETURNING id;", &book.Title, &book.Author, &book.Year, &book.ID)
-
+	result, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4 RETURNING id;", &book.Title, &book.Author, &book.Year, &book.ID)
 	rowsUpdated, err := result.RowsAffected()
 	logFatal(err)
 	json.NewEncoder(w).Encode(rowsUpdated)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	result, err := db.Exec("delete from books where id=$1", params["id"])
+	logFatal(err)
 
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
